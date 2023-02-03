7475d999607f42b7959cf361cbd80517a5974e02
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-03 21:57:07 +0100

    AbstractDao modifications

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -19,7 +19,7 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
         if(AbstractDao.connection==null) {
             try {
                 Properties p = new Properties();
-                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                p.load(ClassLoader.getSystemResource("application.properties.sample").openStream());
                 String url = p.getProperty("db.connection_string");
                 String username = p.getProperty("db.username");
                 String password = p.getProperty("db.password");
