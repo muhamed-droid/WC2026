7a8f5a99186b6790cfc0797848719b4d42cfaa1a
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-07 14:45:06 +0100

    Controller HomeScreen radi što treba da radi

--- src/main/java/ba/unsa/etf/rpr/Controllers/HomeScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/HomeScreenController.java
@@ -1,11 +1,30 @@
 package ba.unsa.etf.rpr.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class HomeScreenController {
 
 
-    public void buttonClick(ActionEvent actionEvent) {
+    public void buttonClick(ActionEvent actionEvent) throws IOException {
 
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TeamsSelectingPage.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("WC2026");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(true);
+        stage.getIcons().add(new Image("/icons/homeScreenIcon.jpg"));
+        //Image icon = new Image(getClass().getResourceAsStream("homeScreenIcon.png"));
+        //primaryStage.getIcons().add(icon);
+        stage.show();
     }
 }
