cc7e5567147ccc37c839a0fd31105ddae80ea9a2
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-07 14:34:04 +0100

    buttonClick action method created

--- src/main/java/ba/unsa/etf/rpr/Controllers/HomeScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/HomeScreenController.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.event.ActionEvent;
+
 public class HomeScreenController {
 
 
+    public void buttonClick(ActionEvent actionEvent) {
 
+    }
 }
