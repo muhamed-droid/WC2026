ddb93989cff24099be4bf29e1e33c1c82022da2b
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 19:35:46 +0100

    GroupsMaking Controller mopdifications

--- src/main/java/ba/unsa/etf/rpr/Controllers/GroupsMakingPageController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/GroupsMakingPageController.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.domain.Team;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Pagination;
+
 public class GroupsMakingPageController {
+
+    @FXML
+    private ListView<Team> listView = new ListView<Team>();
+
+    @FXML
+    private Pagination pagination = new Pagination();
+
+
+
+
 }
