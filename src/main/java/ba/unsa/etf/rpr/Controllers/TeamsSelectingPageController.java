89c58e41d613be37c2c5c2a869e8f0d43812a4e8
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 14:39:12 +0100

    Optimize imports

--- src/main/java/ba/unsa/etf/rpr/Controllers/TeamsSelectingPageController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/TeamsSelectingPageController.java
@@ -9,19 +9,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class TeamsSelectingPageController {
 
