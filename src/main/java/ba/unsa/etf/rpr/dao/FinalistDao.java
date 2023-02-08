d53b652874d1087fe7292d0b87890c4e43a91fc2
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:31:28 +0100

    javadoc corrections spelling

--- src/main/java/ba/unsa/etf/rpr/dao/FinalistDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/FinalistDao.java
@@ -2,6 +2,13 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Finalist;
 
+
+/**
+ * ConfederationDao class, crucial part for dao layer implementation
+ * It's main purpose is to extend interface
+ *
+ * @author Muhamed-droid
+ */
 public interface FinalistDao extends Dao<Finalist> {
 
 }
