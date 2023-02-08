da3b4d1d0bb20492d69080c655fff930ba06e07c
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:34:22 +0100

    GroupDao java doc added

--- src/main/java/ba/unsa/etf/rpr/dao/GroupDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GroupDao.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Group;
 
 
+/**
+ * GroupDao class, crucial part for dao layer implementation
+ * It's main purpose is to extend interface
+ *
+ * @author Muhamed-droid
+ */
+
 public interface GroupDao extends Dao<Group> {
 
 }
