4feb56a8123b5af02341734b04d64b2b12506aa7
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 01:16:02 +0100

    Label bined with choice

--- src/main/java/ba/unsa/etf/rpr/Controllers/TeamsSelectingPageController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/TeamsSelectingPageController.java
@@ -7,7 +7,9 @@ import ba.unsa.etf.rpr.domain.Team;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
 public class TeamsSelectingPageController {
 
@@ -17,6 +19,12 @@ public class TeamsSelectingPageController {
 
     @FXML
     private ChoiceBox<Team> choiceBox2 = new ChoiceBox<Team>();
+
+    @FXML
+    private Button addButton = new Button();
+
+    @FXML
+    private Label selectedTeam = new Label();
 
     @FXML
     public void initialize() {
@@ -30,9 +38,16 @@ public class TeamsSelectingPageController {
 
         choiceBox1.setValue(FXCollections.observableList(confederationManager.getAll()).get(0));
         choiceBox1.setItems(FXCollections.observableList(confederationManager.getAll()));
+
         //choiceBox1.setValue();
         choiceBox2.setItems(FXCollections.observableList(teamManager.searchTeamsInConfederation(choiceBox1.getValue())));
         choiceBox2.setValue(FXCollections.observableList(teamManager.searchTeamsInConfederation(choiceBox1.getValue())).get(0));
+        choiceBox2.setOnAction(this::getTeam);
+    }
+
+     private void getTeam (ActionEvent event) {
+        String selected = choiceBox2.getValue().toString();
+        selectedTeam.setText(selected);
     }
 
     public void onAddButtonClick(ActionEvent actionEvent) {
