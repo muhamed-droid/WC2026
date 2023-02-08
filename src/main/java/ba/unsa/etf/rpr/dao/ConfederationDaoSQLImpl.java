e32be7c0cbc8e0773447c31e094618a08879877f
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 20:30:26 +0100

    CodeCleanup

--- src/main/java/ba/unsa/etf/rpr/dao/ConfederationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ConfederationDaoSQLImpl.java
@@ -12,110 +12,12 @@ import java.util.TreeMap;
 
 public class ConfederationDaoSQLImpl extends AbstractDao<Confederation> implements ConfederationDao {
 
-    /* private Connection connection;
-
-    public ConfederationDaoSQLImpl(){
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_WC2026Base", "freedb_Muhamed-droid", "S#A2S3ceg*ReKGP");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public Confederation getById(int id) {
-        String query = "SELECT * FROM confederations WHERE id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) { // result set is iterator.
-                Confederation confederation = new Confederation();
-                confederation.setId(rs.getInt("id"));
-                confederation.setFullName(rs.getString("full_name"));
-                confederation.setAbbreviation(rs.getString("abbreviation"));
-                rs.close();
-                return confederation;
-            } else {
-                return null; // if there is no elements in the result set return null
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
-    }
-
-
-    @Override
-    public Confederation add(Confederation item) {
-        String insert = "INSERT INTO confederations VALUES(?)";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, item.getFullName());
-            stmt.executeUpdate();
-
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next(); // we know that there is one key
-            item.setId(rs.getInt(1)); //set id to return it back
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-    @Override
-    public Confederation update(Confederation item) {
-        String insert = "UPDATE confederations SET name = ? WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, item.getFullName());
-            stmt.setObject(2, item.getAbbreviation());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
-    @Override
-    public void delete(int id) {
-        //Something will be implemented here
-    }
-
-    @Override
-    public List<Confederation> getAll() {
-        //Something will be implemented here
-        return Collections.emptyList();
-    }
- */
     /**
      * @param id for searching
      * @return specific Confederation for specific id from db
      * @author muhamed-droid
      */
 
-    /*public Confederation returnConfederationForId(int id) {
-        String query = "SELECT * FROM categories WHERE id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                Confederation c = new Confederation();
-                c.setId(rs.getInt(1));
-                c.setFullName(rs.getString(2));
-                c.setAbbreviation(rs.getString(3));
-                return c;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }*/
 
     private static  ConfederationDaoSQLImpl instance = null;
     public ConfederationDaoSQLImpl() {
