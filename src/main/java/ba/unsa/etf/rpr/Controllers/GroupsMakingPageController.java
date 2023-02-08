4fbd6b1ef3bdcbdab2a23aad7b80c5be0adb39fd
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 21:24:56 +0100

    Minor modifications

--- src/main/java/ba/unsa/etf/rpr/Controllers/GroupsMakingPageController.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/GroupsMakingPageController.java
@@ -1,7 +1,9 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.Business.ConfederationManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Team;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Pagination;
@@ -16,6 +18,27 @@ public class GroupsMakingPageController {
 
     @FXML
     public void initialize() {
+
+        ConfederationManager confederationManager = new ConfederationManager();
+
+        Team canada = new Team();
+        canada.setConfederation(FXCollections.observableList(confederationManager.getAll()).get(2));
+        canada.setAbbreviation("CAN");
+        canada.setTeamName("Canada");
+
+        Team mexico = new Team();
+        mexico.setConfederation(FXCollections.observableList(confederationManager.getAll()).get(2));
+        mexico.setAbbreviation("MEX");
+        mexico.setTeamName("Mexico");
+
+        Team usa = new Team();
+        usa.setConfederation(FXCollections.observableList(confederationManager.getAll()).get(2));
+        usa.setAbbreviation("USA");
+        usa.setTeamName("United states");
+
+        listView.getItems().add(canada);
+        listView.getItems().add(mexico);
+        listView.getItems().add(usa);
 
         for (int i = 0; i < DaoFactory.finalistDao().getAll().size(); i++){
             listView.getItems().add(DaoFactory.finalistDao().getAll().get(i).getTeam());
