aaa6fe274fa0961ffeb355d46339d147574241ce
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:36:30 +0100

    TableDao finished

--- src/main/java/ba/unsa/etf/rpr/dao/TableDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TableDao.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Table;
 
+/**
+ * TableDao class, crucial part for dao layer implementation
+ * It's main purpose is to extend interface
+ *
+ * @author Muhamed-droid
+ */
 public interface TableDao extends Dao<Table> {
 }
