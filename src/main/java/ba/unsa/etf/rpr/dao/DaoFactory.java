0bfba4db488c155f8cbc1f6669cabc5b1548be6d
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:30:08 +0100

    javadoc added

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
+/**
+ * Implementation of design pattern
+ * Working with daos in shadow
+ *
+ * @author muhamed-droid
+ */
+
 public class DaoFactory {
     private static final ConfederationDao confederationDao = ConfederationDaoSQLImpl.getInstance();
     private static final GroupDao groupDao = GroupDaoSQLImpl.getInstance();
@@ -7,25 +14,58 @@ public class DaoFactory {
     private static final TeamDao teamDao = TeamDaoSQLImpl.getInstance();
     private static final FinalistDao finalistDao = FinalistDaoSQLImpl.getInstance();
 
+
+    /**
+     * @constructor wihout params
+     *
+     * @author muhamed-droid
+     */
     private DaoFactory(){
     }
 
+
+    /**
+     *
+     * @return ConfederationDao
+     * @author muhamed-droid
+     */
     public static ConfederationDao confederationDao(){
         return confederationDao;
     }
 
+
+    /**
+     *
+     * @return GroupDao
+     * @author muhamed-droid
+     */
     public static GroupDao groupDao(){
         return groupDao;
     }
 
+    /**
+     *
+     * @return TeamDao
+     * @author muhamed-droid
+     */
     public static TeamDao teamDao(){
         return teamDao;
     }
 
+    /**
+     *
+     * @return TableDao
+     * @author muhamed-droid
+     */
     public static TableDao tableDao(){
         return tableDao;
     }
 
+    /**
+     *
+     * @return FinalistDao
+     * @author muhamed-droid
+     */
     public static FinalistDao finalistDao() {return finalistDao;}
 
 }
