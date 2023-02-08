f10032b2ad41825d1ce6730fa2d1dbaca9664116
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-08 21:10:12 +0100

    Some tests written

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,12 +1,6 @@
 
-Z
-*src/test/java/ba/unsa/etf/rpr/AppTest.java,9\b\9bdae70314931d22cd9741c6381ebe4f05ddbbe7
 `
 0src/main/java/ba/unsa/etf/rpr/domain/Idable.java,1\0\1089e0b50000999ff509ccb1c536ba9caa5d3183
-9
-	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 _
 /src/main/java/ba/unsa/etf/rpr/domain/Table.java,9\0\9072058a6b5e1fe09f6ead1be8ec10c5649eff6f
 X
--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,8 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.ConfederationDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.TeamDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Confederation;
+import ba.unsa.etf.rpr.domain.Team;
+import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.assertTrue;
+import java.util.ArrayList;
+
 
 /**
  * Unit test for simple App.
@@ -13,8 +19,36 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void databaseConnectionTest1()
     {
-        assertTrue( true );
+        TeamDaoSQLImpl dao = new TeamDaoSQLImpl();
+        ConfederationDaoSQLImpl dao1 = new ConfederationDaoSQLImpl();
+        Confederation c = dao1.getById(5);
+        ArrayList<Team> teamsByConfederation = new ArrayList<Team>(dao.searchByConfederation(c));
+        Assertions.assertEquals(11, teamsByConfederation.size());
+        Assertions.assertEquals("PNG", teamsByConfederation.get(5).getAbbreviation());
     }
+
+    @Test
+    public void databaseConnectionTest2()
+    {
+        TeamDaoSQLImpl dao = new TeamDaoSQLImpl();
+        ConfederationDaoSQLImpl dao1 = new ConfederationDaoSQLImpl();
+        Confederation c = dao1.getById(5);
+        ArrayList<Team> teamsByConfederation = new ArrayList<Team>(dao.searchByConfederation(c));
+        Assertions.assertEquals("PNG", teamsByConfederation.get(5).getAbbreviation());
+    }
+
+    @Test
+    public void databaseConnectionTest3()
+    {
+        TeamDaoSQLImpl dao = new TeamDaoSQLImpl();
+        ConfederationDaoSQLImpl dao1 = new ConfederationDaoSQLImpl();
+        Confederation c = dao1.getById(5);
+        Assertions.assertEquals("OFC", c.toString());
+    }
+
+
+
+
 }
