f702b9d6ae5b89374ee7aa210f20a02c1ba02994
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:58:54 +0100

    Business directory now complete

--- src/main/java/ba/unsa/etf/rpr/Business/TeamManager.java
+++ src/main/java/ba/unsa/etf/rpr/Business/TeamManager.java
@@ -15,30 +15,62 @@ import java.util.List;
 
 
 public class TeamManager {
+    /**
+     *
+     * @return list of teams
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public List<Team> getAll() throws MyException {
         return DaoFactory.teamDao().getAll();
     }
 
-    public List<Team> searchteams(String name) throws MyException {
-        return DaoFactory.teamDao().searchByName(name);
-    }
-
+    /**
+     * @param con specifies Confederation
+     * @return list of teams
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public List<Team> searchTeamsInConfederation(Confederation con) throws MyException {
         return DaoFactory.teamDao().searchByConfederation(con);
     }
 
+    /**
+     * void function
+     * @param id
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public void delete(int id) throws MyException{
         DaoFactory.teamDao().delete(id);
     }
 
+    /**
+     * @return instance of Team
+     * @param teamId
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public Team getById(int teamId) throws MyException{
         return DaoFactory.teamDao().getById(teamId);
     }
 
+    /**
+     * void function
+     * @param t, instance of team
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public void update(Team t) throws MyException{
         DaoFactory.teamDao().update(t);
     }
 
+    /**
+     * @return Team, instance of team
+     * @param t, instance of team
+     * @throws MyException
+     * @author Muhamed-droid
+     */
     public Team add(Team t) throws MyException{
         return DaoFactory.teamDao().add(t);
     }
