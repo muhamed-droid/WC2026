702ed0474b11e81fcee21988c197a852d611a95a
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:44:14 +0100

    javaDoc added in all classes in exceptions directory

--- src/main/java/ba/unsa/etf/rpr/exceptions/MyException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MyException.java
@@ -1,10 +1,28 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ *Custom Exception made for project
+ *
+ *
+ * @author muhamed-droid
+ */
+
 public class MyException extends RuntimeException{
+    /**
+     *
+     * @return instance of TeamDaoSQLImpl
+     * @author muhamed-droid
+     */
     public MyException(String msg, Exception reason){
         super(msg, reason);
     }
 
+
+    /**
+     *
+     * @return instance of TeamDaoSQLImpl
+     * @author muhamed-droid
+     */
     public MyException(String msg){
         super(msg);
     }
