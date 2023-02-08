6fdcb2a1df963f98f9893707def9f5178a7d2752
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-02-09 00:16:51 +0100

    Javadoc added

--- src/main/java/ba/unsa/etf/rpr/dao/ConfederationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ConfederationDao.java
@@ -2,7 +2,12 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Confederation;
 
-
+/**
+ * ConfederationDao class, crucial part for dao layer implementation
+ * It's main purpose is to extends interface
+ *
+ * @author Muhamed-droid
+ */
 public interface ConfederationDao extends Dao<Confederation> {
 
 }
